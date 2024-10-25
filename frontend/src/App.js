b38d063143b9f8f2ffde9155e619787fc3a4c083
SarveshP12 <sarvesh.2005.1201@gmail.com>
2024-10-26 04:07:45 +0530

    React commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Dashboard from './components/LandingPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Dashboard from './ui/Dashboard';
+import './css/Dashboard.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
